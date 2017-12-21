f1a4ce6cbe7f8f286b5f37f8bcedc33816909cfe
Diogo Ratto <diogoratto.dev@gmail.com>
2017-12-20 23:46:24 -0200

    Adiciona instância MutationObserver para navegadores que dão suporte a mesma

--- 008/tagueamento.js
+++ 008/tagueamento.js
@@ -1,11 +1,29 @@
-(function(window, document, $) {
+(function(window, document) {
   'use strict';
 
-  $('#mensagem').bind('DOMSubtreeModified', function() {
-    var msg = $(this).text().trim();
-    if(!$(this).find('img').length && msg != '') {
-      console.log(msg);
-    }
-  });
+  var observedElement = document.getElementById('mensagem');
+  if ("MutationObserver" in window) {
+        var callback        = function(mutationsList) {
+          for(var i = 0, max = mutationsList.length ; i < max ; i += 1) {
+            if(mutationsList[i].type === 'childList') {
+              var msg = mutationsList[i].addedNodes[0].innerText;
+              if(msg != '') {
+                console.log(msg);
+              }
+            }
+          }
+        },
+        config = {childList: true},
+        observer = new MutationObserver(callback);
 
-})(window, document, jQuery);
+    observer.observe(observedElement, config);
+  }  else {
+    observedElement.addEventListener('DOMSubtreeModified', function() {
+      var msg = this.innerText.trim();
+      if(msg != '') {
+        console.log(msg);
+      }
+    });
+  }
+
+})(window, document);
