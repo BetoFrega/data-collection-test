17e19786b86a026991961276db7539f116f61e68
luancurti <luancurti@gmail.com>
2018-01-06 14:22:45 -0200

    Adicionada resoluçaõ do teste 1

---
+++ 001/tagueamento.js
@@ -0,0 +1,28 @@
+'use strict'
+
+var FATHER_ELEMENTS_ID = 'botoes';
+var BUTTON_ELEMENTS_SELECTOR = '#botoes button';
+var TARGET_NODE_NAME = 'BUTTON';
+
+function init () {
+  console.log(getButtonNames());
+
+  document.getElementById(FATHER_ELEMENTS_ID)
+    .addEventListener('click', logButtonNameOnClick, false);
+}
+
+function getButtonNames () {
+  var buttonElements = document.querySelectorAll(BUTTON_ELEMENTS_SELECTOR);
+
+  return Array.prototype.map.call(buttonElements, getButtonName);
+}
+
+function getButtonName (element) {
+  return element.innerText;
+}
+
+function logButtonNameOnClick ($event) {
+  $event.target && $event.target.nodeName === TARGET_NODE_NAME && console.log($event.target.innerText);
+}
+
+init();
