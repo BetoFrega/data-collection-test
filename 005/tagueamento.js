15d7c9c5ac1e1f89441d717a994c8cca819564a3
luancurti <luancurti@gmail.com>
2018-01-16 00:50:28 -0200

    Corrigido teste 5

--- 005/tagueamento.js
+++ 005/tagueamento.js
@@ -2,30 +2,34 @@
 
 (function () {
   var GREEN_ELEMENTS_SELECTOR = '#valores span';
+  var BUTTONS_SELECTOR = '#botoes a';
   var FATHER_ELEMENTS_ID = 'valores';
+  var BUTTONS_ID = 'botoes';
   var TARGET_NODE_NAME = 'SPAN';
+  var BUTTONS_LIST;
+  var GREEN_ELEMENTS_LIST;
 
   function init () {
+    BUTTONS_LIST = document.querySelectorAll(BUTTONS_SELECTOR);
+    GREEN_ELEMENTS_LIST = document.querySelectorAll(GREEN_ELEMENTS_SELECTOR);
+
     console.log(getGreenElementsText());
 
-    document.getElementById(FATHER_ELEMENTS_ID)
-      .addEventListener('click', logPreviousSiblingElementTextOnClick, false);
+    document.getElementById(BUTTONS_ID).addEventListener('click', logGreenElementTextAbove, false);
   }
 
   function getGreenElementsText () {
-    var greenElements = document.querySelectorAll(GREEN_ELEMENTS_SELECTOR);
-
-    return Array.prototype.map.call(greenElements, getElementText);
+    return Array.prototype.map.call(GREEN_ELEMENTS_LIST, getElementText);
   }
 
   function getElementText (element) {
     return element.innerText;
   }
 
-  function logPreviousSiblingElementTextOnClick ($event) {
-    if ($event.target && $event.target.nodeName === TARGET_NODE_NAME) {
-      console.log($event.target.previousSibling.innerText);
-    }
+  function logGreenElementTextAbove ($event) {
+    var index = Array.prototype.indexOf.call(BUTTONS_LIST, $event.target, 0);
+
+    console.log(GREEN_ELEMENTS_LIST[index].innerText);
   }
 
   init();
