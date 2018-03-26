81f55bbc5cca7f51ee31c2bb41180d46f1aa9a27
Eudes Belmiro <eudes.bbj@gmail.com>
2018-03-26 14:55:13 -0300

    Finalização do teste

---
+++ 002/tagueamento.js
@@ -0,0 +1,27 @@
+var TESTE_02 = {
+	id    : "#valores",
+	botoes : "#botoes",
+	array  : [],	
+	init   : function() {
+		
+		$(TESTE_02.id).find('span').each(function(){
+			TESTE_02.array.push($(this).text());
+		})
+		
+		$(TESTE_02.botoes).find('a').each(function(){
+			$(this).click(function(){
+				TESTE_02.click_botao($(this).index());
+			})
+		})
+		
+		TESTE_02.exibe_valores();
+	},
+	click_botao : function(posicao) {		
+		console.log($(TESTE_02.id + '> span').eq(posicao).text());
+	},
+	exibe_valores : function(){
+		console.log(TESTE_02.array);
+	}
+}
+
+TESTE_02.init();
---
+++ 008/tagueamento.js
@@ -0,0 +1,10 @@
+var TESTE_08 = {
+	id    : "#mensagem",
+	init   : function() {		
+		$(TESTE_08.id).bind('DOMSubtreeModified', function() {
+			console.log($(this).text());
+		});	
+	}
+}
+
+TESTE_08.init();
