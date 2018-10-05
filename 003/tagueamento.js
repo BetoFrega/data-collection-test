782625e6f1a8f91de74b5c6dc57596a63df69274
Elieden Estevam Garcia Barbosa <edengb97@gmail.com>
2018-10-04 22:49:57 -0300

    Update tagueamento.js

--- 003/tagueamento.js
+++ 003/tagueamento.js
@@ -2,11 +2,13 @@
  *   Todos os botões vão possui dois atributo clicado e posicao. 
  *	 Atributo clicado: mostra seu estado atual sendo o estado inicial igual 0 e clicado igual 1.
  *	 Atributo posicao: marca a posicao dos respectivos botões.
- *   O array ordemClick armazena um historico dos clicks temporarialmente até o final do seu preenchimento.
- *   Contudo, assim que preenchido ele deve retorna uma string e reseta o atributo clicado assim como a si mesmo.
+ *   A variável count é apenas um contador que será incrementado após cada alteração no atributo 'clicado'.
+ *   O array ordemClick armazena um historico dos clicks temporarialmente até que todos os botões sejam clicados.
+ * 	 Contudo, quando count for igual a 5 vamos atribuir 0 para OrdemClick, attributo 'Clicado' e a ele mesmo
  */
 var botoes = document.querySelectorAll("button");
 var ordemClick = []; // Historico dos clicks
+var count = 0;
 for(var i = 0; i < botoes.length; i++) {
 
 	botoes[i].setAttribute('clicado', 0); // Atributo criado para marcar se já foi clicado ou não
@@ -16,14 +18,16 @@ for(var i = 0; i < botoes.length; i++) {
 		console.log(this.textContent.substr(position)); // Retorne a última parte (depois do segundo hífen)
 		
 
-		 // Se o elemento ainda não foi clicado, alteramos o atributo clicado e registramos sua posição no historico
+		 // Se o elemento ainda não foi clicado, alteramos o atributo 'clicado' e  incrementamos nosso contador.
 		if( parseInt(this.getAttribute("clicado")) == 0) {
 			this.setAttribute('clicado', 1);
-			ordemClick.push(parseInt(this.getAttribute("posicao")));
+			count++;
+			
 		}
-
+		//Registramos a posição do elemento clicado no array 'ordemClick'
+		ordemClick.push(parseInt(this.getAttribute("posicao")));
 		// Se todos os botões já foram cliados, mostramos uma string com '-' para visualização das sequencias
-		if(ordemClick.length > 4) {
+		if(count >= 5) {
 			console.log(ordemClick.join("-"));
 			
 			// Após o registro ser preenchido começamos tudo novamente
@@ -31,10 +35,10 @@ for(var i = 0; i < botoes.length; i++) {
 			for(var j = 0; j < botoes.length; j++) {
 				botoes[j].setAttribute('clicado', 0);
 			}
+			count = 0;
 		}
 	});
 }
-
 
 
 
