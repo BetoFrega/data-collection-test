c0656258a018c961132b994b758b9ab9d0128113
Elieden Estevam Garcia Barbosa <edengb97@gmail.com>
2018-10-04 22:50:56 -0300

    Update tagueamento.js

--- 004/tagueamento.js
+++ 004/tagueamento.js
@@ -1,6 +1,11 @@
 var botoes = document.querySelectorAll("button");
+var ArrayBotao = [];
 for(var i = 0; i < botoes.length; i++) {
+
+	ArrayBotao.push(botoes[i].textContent);
+
 	botoes[i].addEventListener("click", function(){
 	 console.log(this.textContent)
 	});
 }
+console.log(ArrayBotao);
